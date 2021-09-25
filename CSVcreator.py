b28569c7c86992b4411324b3a9f1cb54e1132a70
Luis Miguel Oviedo L <mimi1296@javerianacali.edu.co>
2021-09-25 15:54:12 -0500

    Update CSVcreator.py

--- CSVcreator.py
+++ CSVcreator.py
@@ -39,7 +39,7 @@ class CSVcreator():
         print("[--INFO--] Creando archivo CSV...")
         with open(self.CVSName, 'w', newline='') as  featuresCSV:
             writer = csv.writer(featuresCSV)
-            writer.writerow(["Name", "zcr", "flt", "lds", "alds", "strpk", "nrg", "cntr", "flu", "rof", "entr", "danc", "bpm", "tufre", "ptch", "tmpo", "edmk", "edm", "mfcc1", "mfcc2", "mfcc3", "mfcc4", "mfcc5, gen"])
+            writer.writerow(["Name", "zcr", "flt", "lds", "alds", "strpk", "nrg", "cntr", "flu", "rof", "entr", "danc", "bpm", "tufre", "ptch", "tmpo", "edmk", "edm", "mfcc1", "mfcc2", "mfcc3", "mfcc4", "mfcc5", "gen"])
             for c in self.cancionPool:
                 writer.writerow([c, str(self.cancionPool[c].zcr), str(self.cancionPool[c].flatness), str(self.cancionPool[c].loudness), str(self.cancionPool[c].average_loudness), str(self.cancionPool[c].s_strongpeak), str(self.cancionPool[c].s_energy), str(self.cancionPool[c].s_centroid), str(self.cancionPool[c].s_flux), str(self.cancionPool[c].s_rolloff), str(self.cancionPool[c].s_entropy), str(self.cancionPool[c].danceability), str(self.cancionPool[c].rythm_bpm), str(self.cancionPool[c].tunning_freq), str(self.cancionPool[c].pitch_salience), str(self.cancionPool[c].tempo), self.cancionPool[c].edmaKey, self.cancionPool[c].edmaScale, str(self.cancionPool[c].mfcc[0]), str(self.cancionPool[c].mfcc[1]), str(self.cancionPool[c].mfcc[2]), str(self.cancionPool[c].mfcc[3]), str(self.cancionPool[c].mfcc[4]), self.cancionPool[c].genero])
         
